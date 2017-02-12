4a885458f3d40761d958a467bcf83601186f30a1
Slatyo <daniel.henze@outlook.com>
2017-02-12 15:44:23 +0100

    add missing files

--- app/ajax.php
+++ app/ajax.php
@@ -1,1 +1,14 @@
 <?php
+use App\Controller as AppController;
+
+// Vendor autoload
+require '../vendor/autoload.php';
+
+// project autoload
+require_once('Autoloader.php');
+\App\Autoloader::register();
+
+$bapiController = new AppController\BapiController();
+$bapiController->getCharacterInformations();
+
+
--- app/autoloader.php
+++ app/autoloader.php
@@ -1,7 +1,28 @@
 <?php
 
 namespace App;
+define('ROOT', dirname(__FILE__).DIRECTORY_SEPARATOR);
 
-class autoloader {
+class Autoloader {
+
+    public function __construct() {
+        spl_autoload_register(array($this, 'loadclass'));
+    }
+
+    public static function register() {
+        new Autoloader();
+    }
+
+    public function loadclass($class_name) {
+        $file = "../".self::setPath($class_name);
+
+        if (file_exists($file)) {
+            require_once($file);
+        }
+    }
     
+    public static function setPath($class_name) {
+        return strtolower(str_replace('\\', '/', $class_name)) . '.php';
+    }
+
 }
